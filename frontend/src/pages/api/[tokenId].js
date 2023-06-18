7a66da6bacf400a9b697a864dc88aa6e6e36c2aa
Ashmit Ranjan <ashmitranjan978@gmail.com>
2023-06-18 17:36:57 +0530

    commit 2

--- frontend/src/pages/api/[tokenId].js
+++ frontend/src/pages/api/[tokenId].js
@@ -5,9 +5,11 @@ export default function handler(req, res) {
 
   const name = `Crypto Dev # ${tokenId}`;
   const description = "CryptoDevs is an NFT Collection for Web3 Developers";
-  const image ="";
+  const image =`https://raw.githubusercontent.com/Ash-mit17/NFTCollection/master/frontend/public/cryptodevs/${Number(tokenId)-1}.svg`;
 
-  res.status(200).json({
-    tokenId : tokenId,
+  return res.json({
+    name:name,
+    description: description,
+    image: image
   })
 }
